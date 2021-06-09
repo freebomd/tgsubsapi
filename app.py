2772a733052d7fe1e3f9dd329c037a2b226d585d
freebomd <76235334+freebomd@users.noreply.github.com>
2021-06-09 08:12:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask,request
 import requests
-import tg
-
+from bs4 import BeautifulSoup as bs
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
 
 @app.route("/")
 def home():
     return "API is working fine"
 
-
-@app.route("/<query>")
-def telegram_(query):
-    return jsonify(tg.telegram(query))
-
+@app.route('/id',methods=['GET','POST'])
+def id():
+    w = request.args.get('name')
+    base_url = f"https://telegram.dog/{w}"
+    r = requests.get(base_url).text
+        #print(r)
+    soup = bs(r,"html")
+        #soup.text
+    members_count = soup.find("div",class_="tgme_page_extra").text.replace(" ","").split("members")[0]
+        #print(members_count)
+    channel_name = soup.find("div", class_="tgme_page_title").text.replace("\n","")
+    dp = soup.find("img",class_="tgme_page_photo_image")['src']
+    data = {}
+    data['status'] = True
+    data['name'] = channel_name #Can be used as public group also
+    data['image'] = dp
+    return data
+@app.errorhandler(500)
+def page_not_found(e):
+    return {'status': False}
 
 if __name__ == "__main__":
     #app.debug = True
